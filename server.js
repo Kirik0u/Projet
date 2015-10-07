51b1e3199c2b6c1a590895b79dee47b47848b613
kleiderer.florian <kleiderer.florian@hotmail.fr>
2015-10-07 15:00:53 +0200

    serveur avec websockets

--- server.js
+++ server.js
@@ -14,6 +14,27 @@ var app = express();
 var httpsServer = https.createServer(credentials, app);
 
 app.use(express.static(__dirname));
+
+// Chargement de socket.io
+var io = require('socket.io').listen(httpsServer);
+
+// Quand on client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket, pseudo) {
+
+	socket.on('nouveau_client', function(pseudo) {
+        	socket.pseudo = pseudo;
+        	socket.broadcast.emit('nouveau_client', pseudo);
+		socket.emit('message', 'Vous êtes bien connecté !');
+		socket.broadcast.emit('message', pseudo+' est en ligne !');
+    		console.log(pseudo+' est connecté !');
+    	});
+	
+	socket.on('disconnect', function(pseudo){
+		socket.broadcast.emit('disconnect', pseudo);
+		socket.broadcast.emit('message', pseudo+' est hors-ligne !');
+		console.log(pseudo+' est deconnecté !');
+	});
+});
 	
 httpsServer.listen(8000);
 console.log('Serveur lancé sur le port 8000');
